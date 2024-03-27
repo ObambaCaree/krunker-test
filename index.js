28941ee14bd8db87f7d55f062085dd29e49d9b5e
ObambaCaree <133160850+ObambaCaree@users.noreply.github.com>
2024-03-27 11:42:57 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const nggUrl = 'https://krunker.io';
+
+const proxy = createProxyMiddleware({
+  target: nggUrl,
+  changeOrigin: true,
+  secure: true,
+  logLevel: 'debug',
+  router: function(req) {
+    if (req.headers.host === 'https://krunker.io') {
+      req.headers['X-Forwarded-For'] = ''; 
+      req.headers['X-Real-IP'] = '';
+      req.headers['Via'] = '';
+    }
+    return nggUrl;
+  }
+});
+
+app.use('/', proxy);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`ObambaKruker is running on port ${port}`);
+});
